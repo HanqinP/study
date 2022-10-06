51bae1ca72e4314e796c980c15921d9ce90d817f
hanqin <phq1027@gmail.com>
2022-10-06 15:32:59 +0800

    20221006

---
+++ algorithm/LeetCode20_ValidParentheses.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LeetCode20_ValidParentheses {
+
+    public boolean isValid(String s) {
+
+        Deque<String> stackList = new ArrayDeque<>();
+        for(int i = 0;i<s.length();i++){
+
+
+
+        }
+
+
+
+        return false;
+    }
+
+
+}
