e35885bebf5f9bfc9042587af8406ac89b13c7ed
hanqin <phq1027@gmail.com>
2023-08-01 23:57:24 +0800

    230801

---
+++ algorithm/Python/LeetCode1_TwoSum.py
@@ -0,0 +1,8 @@
+class ListNode:
+    def __init__(self,x):
+        self.val = x
+        self.next = None
+
+
+if __name__ == '__main__':
+    head1 = ListNode(0)
--- python/main.py
+++ python/main.py
@@ -5,13 +5,34 @@ if run in other's file, __name__ = main(the name of this file)
 """
 
 import PowerSeries
+from typing import List
 
 print(__name__)
 
 
+def longestCommonPrefix(strs: List[str]) -> str:
+    prefix = strs[0]
+    if len(strs) <= 0:
+        return ''
+    for index in range(1,len(strs)):
+        
+        minLength = min(len(prefix),len(strs[index]))
+        for j in range(minLength):
+            if prefix[j] != strs[index][j] :
+                prefix = prefix[:j]
+                break
+        prefix = prefix if len(prefix) < len(strs[index]) else strs[index]
+        
+    return prefix
+
+
+
+
+
+
 def main():
-    print("this function is from main function")
-    #print(PowerSeries.doPowerSeries(0.5))
+    strs = ["ab","a"]
+    print(longestCommonPrefix(strs))
 
 if __name__ == "__main__":
     main()
